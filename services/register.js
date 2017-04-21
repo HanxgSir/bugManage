34d88ec48419e8c7f91d458e4ea15c91ddadc02b
hardaway <18304018283@163.com>
2017-04-21 13:38:58 +0800

    注册用户 注册时判断邮箱和用户名是否重复 加注册成功跳转到登录界面

--- services/register.js
+++ services/register.js
@@ -1,26 +1,41 @@
-// 注册请求
+// 注册请求 判断用户名和邮箱是否重复
 module.exports = function (app) {
     app.post('/register', function (req, res) {
         // 读取user model
         var User = global.dbHelper.getModel('user');
-        // 查询注册用户名是否重复
+        let filter = {
+            name: req.body.nickname,
+
+        };
         User.findOne({name: req.body.nickname}, function (error, doc) {
+            // 查询注册用户名是否重复
             if (doc) {
+                // 用户名重复
                 res.send({status: 1, msg: "用户名已存在"})
             } else {
-                // 如不重复 向model添加新用户
-                User.create({
-                    name: req.body.nickname,
-                    password: req.body.password,
-                    email: req.body.email,
-                    phone: req.body.phone
-                }, function (error, doc) {
-                    if (error) {
-                        res.sendStatus(500);
-                    } else {
-                        res.send({status: 0, msg: "注册成功"});
+                User.find({email: req.body.em}, function (error, doc) {
+                    // 继续查询邮箱是否重复
+                    if (doc) {
+                        // 邮箱重复
+                        res.send({status: 1, msg: "邮箱已经被占用"})
                     }
-                });
+                    else {
+                        // 如用户名和邮箱均不重复 则生成新用户
+                        User.create({
+                            name: req.body.nickname,
+                            password: req.body.password,
+                            email: req.body.email,
+                            phone: req.body.phone
+                        }, function (error, doc) {
+                            if (error) {
+                                res.send({status: 1, msg: "注册失败，服务器错误"});
+                            } else {
+                                res.send({status: 0, msg: "注册成功"});
+                            }
+                        });
+                    }
+                })
+
             }
         });
     });
